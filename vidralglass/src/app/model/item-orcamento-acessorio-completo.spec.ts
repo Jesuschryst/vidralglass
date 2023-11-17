fbc729c06f1028512c8ee28ab228f04ffdf5e051
Jésus <93550538+Jesuschryst@users.noreply.github.com>
2023-11-17 15:16:19 -0300

    Delete vidralglass directory

--- vidralglass/src/app/model/item-orcamento-acessorio-completo.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ItemOrcamentoAcessorioCompleto } from './item-orcamento-acessorio-completo';
-
-describe('ItemOrcamentoAcessorioCompleto', () => {
-  it('should create an instance', () => {
-    expect(new ItemOrcamentoAcessorioCompleto()).toBeTruthy();
-  });
-});
